224e10cd88ab9f9cd3cb19633f30b6da0bccb100
Phatchara_Mix <99187452+MiracleX77@users.noreply.github.com>
2024-02-08 00:24:34 +0700

    Update dentistPostgresRepository.go

--- Backend_Go/dentists/repositories/dentistPostgresRepository.go
+++ Backend_Go/dentists/repositories/dentistPostgresRepository.go
@@ -18,8 +18,8 @@ func NewDentistPostgresRepository(db *gorm.DB) DentistRepository {
 
 func (r *dentistPosgresRepository) InsertDentistData(in *entities.InsertDentist) error {
 	data := &entities.Dentist{
-		FirstName: &in.FirstName,
-		LastName:  &in.LastName,
+		FirstName: in.FirstName,
+		LastName:  in.LastName,
 		Username:  in.Username,
 		Password:  in.Password,
 		CreateOn:  in.CreateOn,
