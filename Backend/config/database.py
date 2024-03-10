68f29353f85aa9417a5513d592229f5cfd6a33fe
ワルンポ-ンさん <tankubopa77@gmail.com>
2024-03-10 15:16:24 +0700

    Update database.py
    test

---
+++ Backend/config/database.py
@@ -0,0 +1,27 @@
+
+import os
+from databases import Database
+
+class DatabaseConnection:
+    def __init__(self):
+        # username = os.getenv("POSTGRES_USER")
+        # password = os.getenv("POSTGRES_PASSWORD")
+        # host = os.getenv("POSTGRES_HOST")
+        # port = os.getenv("POSTGRES_PORT")
+        # database = os.getenv("POSTGRES_DB")
+        # self.url = f"postgresql://{username}:{password}@{host}:{port}/{database}"
+        self.testing_url = f"postgresql://admin:1234@localhost:5432/segmentation"
+        self.database = Database(self.testing_url)
+
+
+    def getDb(self):
+        return self.database
+    
+    async def start(self):
+        await self.database.connect()
+    
+    async def stop(self):
+        await self.database.disconnect()
+
+postgres = DatabaseConnection()
+
